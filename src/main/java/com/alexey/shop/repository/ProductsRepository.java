c4a98d020b9e4d1962a51541f4f10305d970b27e
fant205 <fant205@mail.ru>
2022-11-16 13:18:27 +0300

    Homework 7 - Все пункты выполнил 1. Сделайте страницу для отображения всех товаров 2. Сделать добавление продуктов 3. * Сделать фильтр min/max с помощью формы 4. ** Попробуйте реализовать разбивку всех товаров на страницы, по 10 товаров в каждой. (Page & Pageable)

--- src/main/java/com/alexey/shop/repository/ProductsRepository.java
+++ src/main/java/com/alexey/shop/repository/ProductsRepository.java
@@ -1,6 +1,8 @@
 package com.alexey.shop.repository;
 
 import com.alexey.shop.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,4 +12,8 @@ public interface ProductsRepository extends JpaRepository<Product, Long> {
 
     @Query("select p from Product p where p.cost >= :min and p.cost <= :max")
     List<Product> findProductsBetween(Integer min, Integer max);
+
+    @Query("select p from Product p where p.cost >= :min and p.cost <= :max")
+    Page<Product> findProductsAll(Integer min, Integer max, PageRequest pageRequest);
+
 }
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,16 +1,52 @@
 angular.module('app', []).controller('indexController', function ($scope, $http) {
     const contextPath = 'http://localhost:8189/app';
+    var currentPage = 0;
+    var size = 10;
 
-    $scope.loadProducts = function () {
-        $http.get(contextPath + '/product/all')
-            .then(function (response) {
-                $scope.productList = response.data;
-            });
+    $scope.nextPage = function (i) {
+        currentPage = currentPage + i;
+        $scope.loadProducts(currentPage);
+    };
+
+    $scope.loadProducts = function (page) {
+        if(page == null){
+            page = currentPage;
+        }
+        console.log("page: " + page);
+        var min = 0;
+        var max = 100;
+        if($scope.newFilterJson != null){
+            min = $scope.newFilterJson.min;
+            max = $scope.newFilterJson.max;
+        }
+
+        $http({
+            url: contextPath + '/products/all',
+            method: 'GET',
+            params: {
+                min: min,
+                max: max,
+                page: page,
+                size: size,
+            }
+        }).then(function (response){
+            $scope.productList = response.data.list;
+            var array = [];
+            for (let index = 0; index < response.data.pagesCount; index++) {
+                array.push({
+                    id: index + 1
+                })
+            }
+            $scope.pagesCount = array;
+            console.log($scope.pagesCount);
+            $scope.recordsTotal = response.data.recordsTotal;
+        });
+
     };
 
     $scope.changeScore = function (id, cost){
         $http({
-            url: contextPath + '/product/change_cost',
+            url: contextPath + '/products/change_cost',
             method: 'GET',
             params: {
                 id: id,
@@ -23,16 +59,21 @@ angular.module('app', []).controller('indexController', function ($scope, $http)
 
     $scope.delete = function (id){
         $http({
-            url: contextPath + '/product/delete',
+            url: contextPath + '/products/delete/' + id,
             method: 'DELETE',
-            params: {
-                id: id
-            }
         }).then(function (response){
             $scope.loadProducts();
         });
     };
 
-    $scope.loadProducts();
+    $scope.createProductJson = function (){
+        console.log($scope.newProductJson);
+        $http.post(contextPath + '/products', $scope.newProductJson)
+            .then(function (response) {
+                $scope.loadProducts()
+            });
+    };
+
+    $scope.loadProducts(0);
 
 });
